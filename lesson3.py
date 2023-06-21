cafbb6439abe733fb9749208525fafca2568453e
msxxxxx <max.soroko96@gmail.co>
2023-06-21 19:08:10 +0300

    initial commit

---
+++ lesson3.py
@@ -0,0 +1,16 @@
+# age = 34
+# name = 'Alex'
+# is_human = True
+#
+# text1 = 'Hello' + name + 'age' + str(is_human)
+# text2 = 'Hello %s your age %d is human %d' %(name, age, is_human)
+# text3 = 'Hello {} age {} is human {}'.format(name, age, is_human)
+# text4 = f'Hello {name} age {age} is human {is_human}'
+# print(text1)
+# print(text2)
+# print(text3)
+# print(text4)
+# print(text4)
+# print('\N{fire}')
+# # 3 и 4 часто используются
+
