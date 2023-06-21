097eab966dc2516ed4ba91e2b10aae413e734271
msxxxxx <max.soroko96@gmail.co>
2023-06-21 21:00:01 +0300

    initial commit

--- lesson3.py
+++ lesson3.py
@@ -1,16 +1,43 @@
-# age = 34
-# name = 'Alex'
-# is_human = True
+# # age = 34
+# # name = 'Alex'
+# # is_human = True
+# #
+# # text1 = 'Hello' + name + 'age' + str(is_human)
+# # text2 = 'Hello %s your age %d is human %d' %(name, age, is_human)
+# # text3 = 'Hello {} age {} is human {}'.format(name, age, is_human)
+# # text4 = f'Hello {name} age {age} is human {is_human}'
+# # print(text1)
+# # print(text2)
+# # print(text3)
+# # print(text4)
+# # print(text4)
+# # print('\N{fire}')
+# # # 3 и 4 часто используются
 #
-# text1 = 'Hello' + name + 'age' + str(is_human)
-# text2 = 'Hello %s your age %d is human %d' %(name, age, is_human)
-# text3 = 'Hello {} age {} is human {}'.format(name, age, is_human)
-# text4 = f'Hello {name} age {age} is human {is_human}'
-# print(text1)
+# # print('hello'.center(16,'-'))
+# # print('hello'.zfill(16))
+#
+# sent = input("input 3 word:")
+# print(len(sent))
+# sent_1 = sent.split()
+# print(sent_1[2], sent_1[1], sent_1[0])
+#
+# f_space = sent.find(' ')
+# l_space = sent.rfind(' ')
+# print(f_space, l_space)
+
+#palindrom
+# text = input("input word:")
+# text1 = text.lower()
+# text2 = text1[::-1]
 # print(text2)
-# print(text3)
-# print(text4)
-# print(text4)
-# print('\N{fire}')
-# # 3 и 4 часто используются
+# print(text1 == text2)
+
+# text = input().lower()
+# print(text == text[::-1])
+
+# son = int(input())
+# dad = int(input())
+#
+# print(dad-2*son)
 
