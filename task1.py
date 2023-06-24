68063c5bac6bdbbaaee5bb8bc9e517d8d41892e5
msxxxxx <max.soroko96@gmail.co>
2023-06-24 22:17:51 +0300

    initial commit

--- task1.py
+++
@@ -1,6 +0,0 @@
-#Пользователь вводит предложение, заменить все пробелы на "-" 2-мя
-#способамиi
-sentence = input("Input sentence: ")
-r_sentence = sentence.replace(" ","-")
-print(r_sentence)
-print(sentence.replace(" ","-"))
