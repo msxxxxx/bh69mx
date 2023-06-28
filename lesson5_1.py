166a7650bfbaab8320c2e16767f75e503b8814ed
msxxxxx <max.soroko96@gmail.co>
2023-06-28 20:55:57 +0300

    initial commit

---
+++ lesson5_1.py
@@ -0,0 +1,29 @@
+# # n = int(input('deposit: '))
+# # proc = int(input('procent: '))
+# # proc = proc/100
+# # year = 0
+# # while n < 2*n:
+# #     year += 1
+# #     n *= proc
+# # print(year)
+#
+# amount = int(input('sum: '))
+# coins = [25, 10, 5, 1]
+# count = 0
+# for coin in coins:
+#     count += amount // coin
+#     amount -= (amount // coin) * coin
+# print(count)
+# #
+# #
+#
+
+# list = [i for i in [1, 2, 3, 4, 5, 6, 7, 8, 22] if not i % 2]
+# print(list)
+# numbers = [1, 2, 3, 3, 4, 5, 22, 6, 7, 8, 22]
+# for i in range(len(numbers) -1, -1, -1):
+#     if numbers[i] % 2:
+#         del numbers[i]
+# print(numbers)
+
+
