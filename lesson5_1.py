543db0080157a60c96df5763516b02c188110e86
msxxxxx <max.soroko96@gmail.co>
2023-07-04 16:42:35 +0300

    initial commit

--- lesson5_1.py
+++ lesson5_1.py
@@ -1,17 +1,33 @@
 # Вывести первые N чисел кратные M и больше K
 # n = 5, m = 3, k = 10 : 12,15,18,21,24
 
-c = 0
+# c = 0
+# n = int(input('n = '))
+# m = int(input('m = '))
+# k = int(input('k = '))
+# lst = []
+# while c < k:
+#     c += 1
+#     while c >= k:
+#         c += 1
+#         if c % m == 0:
+#             lst.append(c)
+#             if len(lst) == n:
+#                 break
+# print(lst)
+
 n = int(input('n = '))
 m = int(input('m = '))
 k = int(input('k = '))
+c = k
 lst = []
-while c < k:
+
+while k >= k:
     c += 1
-    while c >= k:
-        c += 1
-        if c % m == 0:
-            lst.append(c)
-            if len(lst) == n:
-                break
+    if c % m == 0:
+        lst.append(c)
+        if len(lst) == n:
+            break
 print(lst)
+
+
