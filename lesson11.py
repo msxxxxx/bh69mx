ef6e8268f3c4ea8d78ef8417bfac8b2919730b58
msxxxxx <max.soroko96@gmail.co>
2023-07-24 01:20:05 +0300

    initial commit

--- lesson11.py
+++ lesson11.py
@@ -1,14 +1,10 @@
-from csv import reader, DictReader, writer, DictWriter
 from pydantic import (
     BaseModel,
-    EmailStr,
-    PostgresDsn,
     Field,
-    field_validator,
-    model_validator,
-    validate_call, ValidationError, PositiveInt
+    field_validator
 )
 from pydantic.types import Decimal, PositiveInt
+from typing import List
 
 # 1.	Написать Pydantic схему для валидации данных:
 # a.	title - строка с длиной не более 128
@@ -16,12 +12,13 @@ from pydantic.types import Decimal, PositiveInt
 # c.	price - Decimal 8/2
 # d.	count - целое положительное число
 
-class Products(BaseModel):
+class SchemaValid(BaseModel):
     title: str
     descr: str
     price: Decimal = Field(max_digits=8, decimal_places=2)
     count: PositiveInt
 
+# city = SchemaValid.model_validate(file)
     @field_validator("title")
     @classmethod
     def validate_title(cls, value):
@@ -46,44 +43,23 @@ class Products(BaseModel):
     def validate_count(cls, value: PositiveInt):
         return value
 
-
-with open('products.csv', 'r', encoding='utf-8') as file:
-    r = DictReader(file, delimiter=',')
-    # for line in r:
-    #     line = dict(line)
-    #     a = Products(**line)
-    #     print(a)
-
 # 2.	Написать абстрактный класс:
 # a.	schema - атрибут класса (объявляется с аннотацией типа без присваивания значения)
 # b.	parse - метод класса принимающий объект файла, разделитель и список
 # c.	dump - метод класса, принимающий список, объект файла и разделитель
+from abc import ABC, abstractmethod
+class AbstractProducts(ABC):
+    schema: List[SchemaValid]
+    def __init__(self, file, delimiter):
+        self.file = file
+        self.delimiter = delimiter
 
-class Abstrat:
-    def __init__(self):
-        self.schema: dict
-
-    def parse(self, file, delimiter, lst):
+    def parse(self):
         pass
 
-    def dump(self, lst, file, delimiter):
+    # @abstractmethod
+    def dump(self):
         pass
-
-    def __init__(self, text: str) -> None:
-        self.data = self.loads(text)
-    @classmethod
-    def loads(cls, text: str) -> dict[str, dict[str, str]]:
-        lines = [line for line in text.split('\n') if line]
-        data = {}
-        current_section = ''
-        for line in lines:
-            if line.startswith('[') and line.endswith(']'):
-                current_section = line[1:-1]
-                data[current_section] = {}
-            else:
-                key, value = line.split('=')
-                data[current_section][key] = value
-        return data
 
 # 3.	На основании абстрактного класса из пункта 2, создать дочерний класс:
 # a.	schema - присвоить в качестве значения ссылку на Pydantic схему из пункта 1
@@ -94,15 +70,40 @@ class Abstrat:
 # Pydantiс схемы, объект файла и разделитель и записывающий
 # данные из схем в csv файл (все схемы в списке должны быть экземплярами
 # схемы указанной в атрибуте класса schema, если одна из схем не является, вызывать исключение TypeError)
+list_of_dictionaries = []
+class Products(AbstractProducts):
+    schema: SchemaValid
 
-class Children(Abstrat):
-    schema = Products()
+    def parse(self):
 
-    def parse(self, file, delimiter):
-        return self.schema
+        with open(self.file) as infile:
+            column_names = infile.readline()
+            keys = column_names.strip().split(self.delimiter)
+            number_of_columns = len(keys)
+            data = infile.readlines()
+            list_of_rows = []
+            for row in data:
+                list_of_rows.append(row.strip().split(self.delimiter))
+            infile.close()
+            for item in list_of_rows:
+                row_as_a_dictionary = {}
+                for i in range(number_of_columns):
+                    row_as_a_dictionary[keys[i]] = item[i]
+                list_of_dictionaries.append(row_as_a_dictionary)
+        for items in list_of_dictionaries:
+            try:
+                s = SchemaValid(**items)
+            except:
+                pass
 
-    def dump(self, lst, file, delimiter):
-        return lst, file, delimiter
+    def dumps(self, list_of_dictionaries):
+        with open(self.file, 'w') as f:
+            f.write(self.delimiter.join(list_of_dictionaries[0].keys()))
+            f.write('\n')
+            for row in list_of_dictionaries:
+                f.write(self.delimiter.join(str(x) for x in row.values()))
+                f.write('\n')
+        return True
 
-
-
+b = Products(file="products.csv", delimiter=',').parse()
+lol = Products(file="pr_output.csv", delimiter=',').dumps(list_of_dictionaries=list_of_dictionaries)
---
+++ test.py
@@ -0,0 +1,43 @@
+def parse(fileName, delimiter):
+    with open(fileName) as infile:
+        column_names = infile.readline()
+        keys = column_names.strip().split(delimiter)
+        number_of_columns = len(keys)
+        list_of_dictionaries = []
+        data = infile.readlines()
+        list_of_rows = []
+        for row in data:
+            list_of_rows.append(row.strip().split(delimiter))
+        infile.close()
+        for item in list_of_rows:
+            row_as_a_dictionary = {}
+            for i in range(number_of_columns):
+                row_as_a_dictionary[keys[i]] = item[i]
+            list_of_dictionaries.append(row_as_a_dictionary)
+        return list_of_dictionaries
+        # запихнуть валидатор a = Schema(**line)
+
+    # for i in range(len(list_of_dictionaries)):
+    #     print(list_of_dictionaries[i])
+
+a = parse(fileName="products.csv", delimiter=',')
+print(a)
+lst = [{'title': 'banan', 'descr': 'fruit', 'price': '3.34', 'count': '10'}, {'title': 'orange', 'descr': 'fruit', 'price': '4.33', 'count': '20'}, {'title': 'limon', 'descr': 'fruit', 'price': '4.35', 'count': '5'}]
+def dumps(fileName, delimiter, lst_of_dictionaries):
+    with open(fileName, 'w') as f:
+        f.write(delimiter.join(lst_of_dictionaries[0].keys()))
+        f.write('\n')
+        for row in lst_of_dictionaries:
+            f.write(delimiter.join(str(x) for x in row.values()))
+            f.write('\n')
+    return True
+
+b = dumps(fileName="pr_output.csv", delimiter=',', lst_of_dictionaries=lst)
+print(b)
+
+# with open('products.csv', 'r', encoding='utf-8') as file:
+#     r = DictReader(file, delimiter=',')
+#     for line in r:
+#         line = dict(line)
+#         a = Schema(**line)
+#         print(a)
